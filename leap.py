2428a8aa0038bb653998a6df794c2a91eacdcfd7
Danielsamra <153258404+Danielsamra@users.noreply.github.com>
2023-12-08 10:42:12 +0530

    Create leap.py

---
+++ leap.py
@@ -0,0 +1,17 @@
+def is_leap_year(year):
+    # Leap year is divisible by 4
+    if year % 4 == 0:
+        # If divisible by 100, it should also be divisible by 400
+        if year % 100 == 0:
+            return year % 400 == 0
+        else:
+            return True
+    else:
+        return False
+
+# Example usage:
+year_to_check = 2024
+if is_leap_year(year_to_check):
+    print(f"{year_to_check} is a leap year.")
+else:
+    print(f"{year_to_check} is not a leap year.")
